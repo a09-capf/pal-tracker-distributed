6b54dc09eee50eff4ad5084325fe8de20435801b
miyohide <miyohide@gmail.com>
2020-01-31 17:10:52 +0900

    use config-server

--- integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
+++ integration-test/src/test/java/test/pivotal/pal/tracker/support/ApplicationServer.java
@@ -37,6 +37,7 @@ public class ApplicationServer {
 
         start(envMapBuilder()
             .put("SPRING_DATASOURCE_URL", dbUrl)
+                .put("REGISTRATION_SERVER_ENDPOINT", "http://registration-server")
                 .put("EUREKA_CLIENT_ENABLED", "false")
                 .put("RIBBON_EUREKA_ENABLED", "false")
                 .put("APPLICATION_OAUTH_ENABLED", "false")
